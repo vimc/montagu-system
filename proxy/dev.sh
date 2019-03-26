d6a10caef8be778cbc10d6ec511a308797849353
Emma Russell <EmmaRussellKilburn@gmail.com>
2019-03-26 15:09:09 +0000

    Added portals and static to proxy network, and make generating test data optional

--- proxy/dev.sh
+++ proxy/dev.sh
@@ -2,29 +2,62 @@
 set -e
 
 export TOKEN_KEY_PATH=$PWD/token_key
+export REGISTRY=docker.montagu.dide.ic.ac.uk:5000
 
-rm -rf workspace
-docker-compose --project-name montagu down
+# Pull down old containers
+rm -rf workspace || true
 docker stop reverse-proxy || true
 docker rm reverse-proxy || true
 docker stop montagu-metrics || true
 docker rm montagu-metrics || true
+docker-compose --project-name montagu down
 docker network rm montagu_proxy || true
 
 echo "Generating SSL keypair"
 mkdir workspace
 docker run --rm \
     -v $PWD/workspace:/workspace \
-    docker.montagu.dide.ic.ac.uk:5000/montagu-cert-tool:master \
+    $REGISTRY/montagu-cert-tool:master \
     gen-self-signed /workspace > /dev/null 2> /dev/null
-
-#docker network create montagu_proxy
 
 # Run up all the APIs and Portals which are to be proxied
 docker volume rm montagu_orderly_volume -f
 docker-compose pull
 docker-compose --project-name montagu up -d
 
+# Start the APIs
+docker exec montagu_api_1 mkdir -p /etc/montagu/api/
+docker exec montagu_api_1 touch /etc/montagu/api/go_signal
+docker exec montagu_reporting_api_1 mkdir -p /etc/montagu/reports_api
+docker exec montagu_reporting_api_1 touch /etc/montagu/reports_api/go_signal
+docker exec montagu_orderly_1 touch /orderly_go
+
+# Wait for the database
+docker exec montagu_db_1 montagu-wait.sh
+
+# Migrate the database
+migrate_image=$REGISTRY/montagu-migrate:master
+docker pull $migrate_image
+docker run --network=montagu_proxy $migrate_image
+
+# Generate test data, including test users and reports, if 'data' present as first param
+if [ "$1" = "data" ]; then
+  test_data_image=$REGISTRY/montagu-generate-test-data:master
+  docker pull $test_data_image
+  docker run --rm --network=montagu_proxy $test_data_image
+
+  # Generate report test data
+    docker pull $REGISTRY/orderly:master
+    docker run --rm \
+      --entrypoint create_orderly_demo.sh \
+      -v montagu_orderly_volume:/orderly \
+      $REGISTRY/orderly:master \
+    /orderly
+
+  docker exec montagu_reporting_api_1 sh -c 'cp /orderly/demo/. /orderly/ -r'
+fi
+
+# Build and run the proxy and metrics containers
 docker build -t reverse-proxy .
 docker run -d \
 	-p "443:443" -p "80:80" \
