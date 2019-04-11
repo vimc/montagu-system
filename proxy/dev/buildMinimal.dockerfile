08b0b529e689b79cbd8ce111e47ca409d6aa9c74
Alex <alex.hill@gmail.com>
2019-04-11 17:42:37 +0100

    remove ssl

--- proxy/dev/buildMinimal.dockerfile
+++ proxy/dev/buildMinimal.dockerfile
@@ -7,11 +7,7 @@ ENV MONTAGU_GIT_ID=$MONTAGU_GIT_ID
 ENV MONTAGU_GIT_BRANCH=$MONTAGU_GIT_BRANCH
 
 RUN rm /workspace/Dockerfile
-COPY dev/minimal.Dockerfile /workspace/Dockerfile
+COPY dev/Dockerfile /workspace/Dockerfile
 COPY dev/nginx.minimal.conf /workspace/nginx.montagu.conf
-
-COPY secrets/certificate.pem secrets/certificate.pem
-COPY secrets/dhparam.pem secrets/dhparam.pem
-COPY secrets/ssl_key.pem secrets/ssl_key.pem
 
 CMD ./dev/build-minimal-image.sh $MONTAGU_GIT_BRANCH $MONTAGU_GIT_ID
--- proxy/dev/run-build-minimal.sh
+++ proxy/dev/run-build-minimal.sh
@@ -5,22 +5,6 @@ git_id=$(git rev-parse --short=7 HEAD)
 git_branch=$(git symbolic-ref --short HEAD)
 REGISTRY=docker.montagu.dide.ic.ac.uk:5000
 
-function cleanup() {
-    rm -rf secrets || true
-}
-
-trap cleanup EXIT
-
-echo "Generating SSL keypair"
-mkdir secrets
-docker run --rm \
-    -v $PWD/secrets:/workspace \
-    $REGISTRY/montagu-cert-tool:master \
-    gen-self-signed /workspace > /dev/null 2> /dev/null
-
-# the real dhparam will be 4096 bits but that takes ages to generate
-openssl dhparam -out secrets/dhparam.pem 1024
-
 docker build -f dev/buildMinimal.dockerfile \
     -t montagu-reverse-proxy-build-minimal-env \
     --build-arg MONTAGU_GIT_ID=$git_id \
