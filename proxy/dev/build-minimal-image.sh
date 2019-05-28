6010d00accc2cefc6d7bc5106f91193a7e5d706e
Alex <alex.hill@gmail.com>
2019-05-28 21:41:45 +0100

    push to docker hub if on master

--- proxy/dev/build-minimal-image.sh
+++ proxy/dev/build-minimal-image.sh
@@ -1,12 +1,21 @@
 #!/usr/bin/env bash
 set -ex
 
+registry_public=vimc
 registry=docker.montagu.dide.ic.ac.uk:5000
 name=montagu-reverse-proxy-minimal
 
 commit_tag=$registry/$name:$MONTAGU_GIT_ID
 branch_tag=$registry/$name:$MONTAGU_GIT_BRANCH
 
+public_branch_tag=$registry_public/$name:$MONTAGU_GIT_BRANCH
+
 docker build -t $commit_tag -t $branch_tag .
 docker push $commit_tag
 docker push $branch_tag
+
+if [[ $MONTAGU_GIT_BRANCH -eq "master" ]]
+then
+    docker tag $commit_tag $public_branch_tag
+    docker push $public_branch_tag
+fi
