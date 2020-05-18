0d073fa8dc5df2a34408dc4c7d248b218466ae4a
Emma Russell <EmmaRussellKilburn@gmail.com>
2020-05-18 10:40:57 +0100

    Always generate orderly db when run deps

--- proxy/scripts/run-dependencies.sh
+++ proxy/scripts/run-dependencies.sh
@@ -34,27 +34,28 @@ migrate_image=$REGISTRY/montagu-migrate:master
 docker pull $migrate_image
 docker run --network=montagu_proxy $migrate_image
 
-# Generate test data, including reports, if 'data' present as first param
-if [ "$1" = "data" ]; then
-  test_data_image=$REGISTRY/montagu-generate-test-data:master
-  docker pull $test_data_image
-  docker run --rm --network=montagu_proxy $test_data_image
+# Generate montagu test data
+test_data_image=$REGISTRY/montagu-generate-test-data:master
+docker pull $test_data_image
+docker run --rm --network=montagu_proxy $test_data_image
 
-  # Generate report test data
-    docker pull $REGISTRY/orderly:master
-    docker run --rm \
-      --entrypoint create_orderly_demo.sh \
-      -v montagu_orderly_volume:/orderly \
-      $REGISTRY/orderly:master \
-    /orderly
-
-fi
+# Generate report test data
+rm demo -rf
+rm git -rf
+docker pull $REGISTRY/orderly:master
+docker run --rm \
+  --entrypoint create_orderly_demo.sh \
+  -u $UID \
+  -v $PWD:/orderly \
+  -w "/orderly" \
+  $REGISTRY/orderly:master \
+  "."
 
 # Migrate the orderlyweb tables
 ow_migrate_image=$REGISTRY/orderlyweb-migrate:master
 docker pull $ow_migrate_image
 docker run --rm --network=montagu_proxy \
-  -v montagu_orderly_volume:/orderly \
+  -v $PWD/demo:/orderly \
   $ow_migrate_image
 
 # Add test user
