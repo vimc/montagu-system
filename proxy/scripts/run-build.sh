b1ac73777ed5c152af6adab3b6029fa6aaf0f5f2
Alex <alex.hill@gmail.com>
2020-08-18 17:24:17 +0100

    correct docker auth path

--- proxy/scripts/run-build.sh
+++ proxy/scripts/run-build.sh
@@ -3,9 +3,16 @@ set -ex
 HERE=$(dirname $0)
 . $HERE/common
 
+# The main build env which tests and builds below
+docker build -f build.dockerfile \
+    -t $BUILD_ENV_TAG \
+    --build-arg MONTAGU_GIT_ID=$GIT_SHA \
+    --build-arg MONTAGU_GIT_BRANCH=$GIT_BRANCH \
+    .
+
 # This is the path for teamcity agents. If running locally, pass in your own docker config location
 # i.e. /home/{user}/.docker/config.json
-docker_auth_path=${1:-/opt/teamcity-agent/.docker/config.json}
+docker_auth_path=${1:-/var/lib/buildkite-agent/.docker/config.json}
 
 docker run \
     -v $docker_auth_path:/root/.docker/config.json \
