274ce97aacb2939a7ff67b8384edb833bf45cc4d
Alex <alex.hill@gmail.com>
2023-02-14 13:29:31 +0000

    update driver

--- proxy/scripts/install-chromedriver.sh
+++ proxy/scripts/install-chromedriver.sh
@@ -3,7 +3,7 @@
 sudo apt-get update
 sudo apt-get install -y unzip xvfb libxi6 libgconf-2-4
 
-wget https://chromedriver.storage.googleapis.com/88.0.4324.96/chromedriver_linux64.zip
+wget https://chromedriver.storage.googleapis.com/110.0.5481.77/chromedriver_linux64.zip
 unzip chromedriver_linux64.zip
 sudo mv chromedriver /usr/bin/chromedriver
 sudo chown root:root /usr/bin/chromedriver
