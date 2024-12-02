600b46bbf4ef60a7deb17e4172ad72cdcf7ed761
Paul Liétar <pl2113@ic.ac.uk>
2024-12-02 16:05:35 +0000

    Update nginx exporter

--- deploy/tests/test_constellation.py
+++ deploy/tests/test_constellation.py
@@ -10,137 +10,151 @@ def test_start_and_stop():
     cfg = MontaguConfig("config/basic")
     obj = montagu_constellation(cfg)
 
-    obj.start()
+    try:
+        obj.start()
 
-    cl = docker.client.from_env()
+        cl = docker.client.from_env()
 
-    assert docker_util.network_exists(cfg.network)
-    assert docker_util.volume_exists(cfg.volumes["db"])
-    assert docker_util.volume_exists(cfg.volumes["burden_estimates"])
-    assert docker_util.volume_exists(cfg.volumes["emails"])
-    assert docker_util.volume_exists(cfg.volumes["mq"])
-    assert docker_util.volume_exists(cfg.volumes["templates"])
-    assert docker_util.volume_exists(cfg.volumes["guidance"])
+        assert docker_util.network_exists(cfg.network)
+        assert docker_util.volume_exists(cfg.volumes["db"])
+        assert docker_util.volume_exists(cfg.volumes["burden_estimates"])
+        assert docker_util.volume_exists(cfg.volumes["emails"])
+        assert docker_util.volume_exists(cfg.volumes["mq"])
+        assert docker_util.volume_exists(cfg.volumes["templates"])
+        assert docker_util.volume_exists(cfg.volumes["guidance"])
 
-    assert docker_util.container_exists("montagu-api")
-    assert docker_util.container_exists("montagu-db")
-    assert docker_util.container_exists("montagu-proxy")
-    assert docker_util.container_exists("montagu-proxy-metrics")
-    assert docker_util.container_exists("montagu-admin")
-    assert docker_util.container_exists("montagu-contrib")
-    assert docker_util.container_exists("montagu-mq")
-    assert docker_util.container_exists("montagu-flower")
-    assert docker_util.container_exists("montagu-task-queue")
-    assert docker_util.container_exists("montagu-fake-smtp")
+        assert docker_util.container_exists("montagu-api")
+        assert docker_util.container_exists("montagu-db")
+        assert docker_util.container_exists("montagu-proxy")
+        assert docker_util.container_exists("montagu-proxy-metrics")
+        assert docker_util.container_exists("montagu-admin")
+        assert docker_util.container_exists("montagu-contrib")
+        assert docker_util.container_exists("montagu-mq")
+        assert docker_util.container_exists("montagu-flower")
+        assert docker_util.container_exists("montagu-task-queue")
+        assert docker_util.container_exists("montagu-fake-smtp")
 
-    containers = cl.containers.list()
-    assert len(containers) == 10
+        containers = cl.containers.list()
+        print([c.name for c in containers])
+        assert len(containers) == 10
 
-    obj.stop(kill=True, remove_volumes=True)
+    finally:
+        obj.stop(kill=True, remove_volumes=True)
 
 
 def test_api_configured():
     cfg = MontaguConfig("config/basic")
     obj = montagu_constellation(cfg)
 
-    obj.start()
+    try:
+        obj.start()
 
-    api = get_container(cfg, "api")
-    api_config = docker_util.string_from_container(api, "/etc/montagu/api/config.properties").split("\n")
+        api = get_container(cfg, "api")
+        api_config = docker_util.string_from_container(api, "/etc/montagu/api/config.properties").split("\n")
 
-    assert "app.url=https://localhost/api" in api_config
-    assert "db.host=db" in api_config
-    assert "db.username=api" in api_config
-    assert "db.password=apipassword" in api_config
-    assert "allow.localhost=False" in api_config
-    assert "upload.dir=/upload_dir" in api_config
-    assert "email.mode=real" not in api_config
+        assert "app.url=https://localhost/api" in api_config
+        assert "db.host=db" in api_config
+        assert "db.username=api" in api_config
+        assert "db.password=apipassword" in api_config
+        assert "allow.localhost=False" in api_config
+        assert "upload.dir=/upload_dir" in api_config
+        assert "email.mode=real" not in api_config
 
-    res = http_get("https://localhost/api/v1")
-    assert '"status": "success"' in res
-
-    obj.stop(kill=True, remove_volumes=True)
+        res = http_get("https://localhost/api/v1")
+        assert '"status": "success"' in res
+    finally:
+        obj.stop(kill=True, remove_volumes=True)
 
     cfg = MontaguConfig("config/complete")
     obj = montagu_constellation(cfg)
 
-    obj.start()
-    api = get_container(cfg, "api")
-    api_config = docker_util.string_from_container(api, "/etc/montagu/api/config.properties").split("\n")
-    assert "email.mode=real" in api_config
-    assert "email.password=changeme" in api_config
-    assert "flow.url=fakeurl" in api_config
+    try:
+        obj.start()
+        api = get_container(cfg, "api")
+        api_config = docker_util.string_from_container(api, "/etc/montagu/api/config.properties").split("\n")
+        assert "email.mode=real" in api_config
+        assert "email.password=changeme" in api_config
+        assert "flow.url=fakeurl" in api_config
 
-    obj.stop(kill=True, remove_volumes=True)
+    finally:
+        obj.stop(kill=True, remove_volumes=True)
 
 
 def test_proxy_configured_self_signed():
     cfg = MontaguConfig("config/basic")
     obj = montagu_constellation(cfg)
 
-    obj.start()
+    try:
+        obj.start()
 
-    api = get_container(cfg, "proxy")
-    cert = docker_util.string_from_container(api, "/etc/montagu/proxy/certificate.pem")
-    key = docker_util.string_from_container(api, "/etc/montagu/proxy/ssl_key.pem")
-    param = docker_util.string_from_container(api, "/etc/montagu/proxy/dhparam.pem")
-    assert cert is not None
-    assert key is not None
-    assert param is not None
+        api = get_container(cfg, "proxy")
+        cert = docker_util.string_from_container(api, "/etc/montagu/proxy/certificate.pem")
+        key = docker_util.string_from_container(api, "/etc/montagu/proxy/ssl_key.pem")
+        param = docker_util.string_from_container(api, "/etc/montagu/proxy/dhparam.pem")
+        assert cert is not None
+        assert key is not None
+        assert param is not None
 
-    res = http_get("https://localhost")
-    assert "Montagu" in res
+        res = http_get("https://localhost")
+        assert "Montagu" in res
 
-    obj.stop(kill=True, remove_volumes=True)
+    finally:
+        obj.stop(kill=True, remove_volumes=True)
 
 
 def test_db_configured():
     cfg = MontaguConfig("config/complete")
     obj = montagu_constellation(cfg)
 
-    obj.start()
+    try:
+        obj.start()
 
-    db = get_container(cfg, "db")
-    res = docker_util.exec_safely(db, f'psql -U {cfg.db_root_user} -d postgres -c "\\du"')
-    res = res.output.decode("UTF-8")
+        db = get_container(cfg, "db")
+        res = docker_util.exec_safely(db, f'psql -U {cfg.db_root_user} -d postgres -c "\\du"')
+        res = res.output.decode("UTF-8")
 
-    for u in cfg.db_users:
-        assert u in res
+        for u in cfg.db_users:
+            assert u in res
 
-    query = "SELECT * FROM pg_replication_slots WHERE slot_name = 'barman'"
-    res = docker_util.exec_safely(db, f'psql -U {cfg.db_root_user} -d postgres -c "{query}"')
-    res = res.output.decode("UTF-8")
+        query = "SELECT * FROM pg_replication_slots WHERE slot_name = 'barman'"
+        res = docker_util.exec_safely(db, f'psql -U {cfg.db_root_user} -d postgres -c "{query}"')
+        res = res.output.decode("UTF-8")
 
-    assert "barman" in res
+        assert "barman" in res
 
-    obj.stop(kill=True, remove_volumes=True)
+    finally:
+        obj.stop(kill=True, remove_volumes=True)
 
 
 def test_proxy_configured_ssl():
     cfg = MontaguConfig("config/complete")
     obj = montagu_constellation(cfg)
 
-    obj.start()
+    try:
+        obj.start()
 
-    api = get_container(cfg, "proxy")
-    cert = docker_util.string_from_container(api, "/etc/montagu/proxy/certificate.pem")
-    key = docker_util.string_from_container(api, "/etc/montagu/proxy/ssl_key.pem")
-    param = docker_util.string_from_container(api, "/etc/montagu/proxy/dhparam.pem")
-    assert cert == "cert"
-    assert key == "k3y"
-    assert param == "param"
+        api = get_container(cfg, "proxy")
+        cert = docker_util.string_from_container(api, "/etc/montagu/proxy/certificate.pem")
+        key = docker_util.string_from_container(api, "/etc/montagu/proxy/ssl_key.pem")
+        param = docker_util.string_from_container(api, "/etc/montagu/proxy/dhparam.pem")
+        assert cert == "cert"
+        assert key == "k3y"
+        assert param == "param"
 
-    obj.stop(kill=True, remove_volumes=True)
+    finally:
+        obj.stop(kill=True, remove_volumes=True)
 
 
 def test_metrics():
     cfg = MontaguConfig("config/basic")
     obj = montagu_constellation(cfg)
 
-    obj.start()
-    http_get("http://localhost:9113/metrics")
+    try:
+        obj.start()
+        http_get("http://localhost:9113/metrics")
 
-    obj.stop(kill=True)
+    finally:
+        obj.stop(kill=True)
 
 
 def get_container(cfg, name):
