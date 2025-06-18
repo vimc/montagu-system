6aa368dc0ac0dc4c545dfdfe547b24aeab161421
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2025-06-18 10:06:52 +0100

    Rename packit containers to drop the orderly-web prefix

--- proxy/scripts/packit-create-test-user.sh
+++ proxy/scripts/packit-create-test-user.sh
@@ -8,4 +8,4 @@ EMAIL='test.user@example.com'
 DISPLAY_NAME='Test User'
 ROLE='ADMIN'
 
-docker exec montagu-orderly-web-packit-db-1 create-preauth-user --username "$USERNAME" --email "$EMAIL" --displayname "$DISPLAY_NAME" --role "$ROLE"
+docker exec montagu-packit-packit-db-1 create-preauth-user --username "$USERNAME" --email "$EMAIL" --displayname "$DISPLAY_NAME" --role "$ROLE"
