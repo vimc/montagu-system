f0aba0d4c9bc75b78d717d6120d353699134c921
Emma <EmmaRussellKilburn@gmail.com>
2025-08-28 00:18:16 +0100

    use local outpack root on bk

--- proxy/scripts/run-integration-tests.sh
+++ proxy/scripts/run-integration-tests.sh
@@ -14,17 +14,17 @@ trap cleanup EXIT
 
 # We need to have an orderly demo folder available for outpack_server container to become available - see README for
 # suggestion for local setup. If we're on Buildkit, write out a minimal config and the server should spin up
-if [[ "$BUILDKITE" = "true" ]]; then
-    OUTPACK_ROOT=$HERE/../../packit/demos/orderly/.outpack
-    OUTPACK_CONFIG_FILE=$OUTPACK_ROOT/config.json
-    if [ ! -f $OUTPACK_CONFIG_FILE ]; then
-        echo "Testing local mkdir"
-        mkdir -p $HERE/testdir || true
-        echo "Creating outpack config file"
-        mkdir -p $OUTPACK_ROOT || true
-        echo '{"core":{"path_archive":null,"use_file_store":true,"require_complete_tree":true,"hash_algorithm":"sha256"},"location":[{"name":"local","type":"local","args":{}}]}' > $OUTPACK_CONFIG_FILE || true
-    fi
-fi
+#if [[ "$BUILDKITE" = "true" ]]; then
+#    OUTPACK_ROOT=$HERE/../../packit/demos/orderly/.outpack
+#    OUTPACK_CONFIG_FILE=$OUTPACK_ROOT/config.json
+#    if [ ! -f $OUTPACK_CONFIG_FILE ]; then
+#        echo "Testing local mkdir"
+#        mkdir -p $HERE/testdir || true
+#        echo "Creating outpack config file"
+#        mkdir -p $OUTPACK_ROOT || true
+#        echo '{"core":{"path_archive":null,"use_file_store":true,"require_complete_tree":true,"hash_algorithm":"sha256"},"location":[{"name":"local","type":"local","args":{}}]}' > $OUTPACK_CONFIG_FILE || true
+#    fi
+#fi
 
 mkdir montagu_emails
 $HERE/run-dependencies.sh
