28469202905bd175c8c99fc3eef00cb20586ab58
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2025-09-16 12:28:45 +0100

    Delete old scripts

--- db/scripts/run.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -ex
-docker build --tag montagu-db .
-docker run --rm -d -p 5432:5432 --name montagu-db montagu-db
-
-docker build --tag montagu-migrate --file migrations/Dockerfile .
-docker run --network=host montagu-migrate migrate -url=jdbc:postgresql://localhost/montagu
-
-watch docker logs montagu-db
