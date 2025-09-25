6125c31b5ba60a38382e5b6774f01d1010328e13
Emma <EmmaRussellKilburn@gmail.com>
2025-09-25 13:54:21 +0100

    make cli test script clearer

--- api/scripts/test-cli.sh
+++ api/scripts/test-cli.sh
@@ -4,4 +4,5 @@ HERE=$(dirname $0)
 . $HERE/common
 
 CLI_IMAGE=$1
-docker run --network $NETWORK $CLI_IMAGE add "test" "test" "test" "test"
+# Add user through the CLI
+docker run --network $NETWORK $CLI_IMAGE add "CLI Test User" "cli.test.user" "cli.test@user.com" "test-password"
