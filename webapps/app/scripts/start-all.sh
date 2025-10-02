607866c135ced689a2686802cc35b064d58cbdb7
Emma <EmmaRussellKilburn@gmail.com>
2025-10-02 15:15:08 +0100

    small tidy ups

--- webapps/app/scripts/start-all.sh
+++ webapps/app/scripts/start-all.sh
@@ -6,26 +6,7 @@ export MONTAGU_API_VERSION=$(<$here/../../config/api_version)
 export MONTAGU_DB_VERSION=$(<$here/../../config/db_version)
 export ROOT=$(realpath $here/../..)
 
-#ORDERLY_IMAGE="vimc/orderly:master"
-#OW_MIGRATE_IMAGE="vimc/orderlyweb-migrate:master"
-#OW_CLI_IMAGE="vimc/orderly-web-user-cli:master"
 MONTAGU_MIGRATE_IMAGE="ghcr.io/vimc/montagu-migrate:$MONTAGU_DB_VERSION"
-
-#if [[ -d $ROOT/demo ]]
-#then
-#  echo "Orderly demo folder already exists, not re-creating it."
-#else
-#  docker pull $ORDERLY_IMAGE
-#  docker run --rm --entrypoint create_orderly_demo.sh -v "$ROOT:/orderly" -u $UID -w /orderly -e HOME=/tmp $ORDERLY_IMAGE .
-#fi
-
-# migrate to add orderlyweb tables
-#docker pull $OW_MIGRATE_IMAGE
-#docker run --rm -v "$ROOT/demo:/orderly" $OW_MIGRATE_IMAGE
-
-# add users manage permission to test user for Orderly Web
-#docker run -v $ROOT/demo:/orderly $OW_CLI_IMAGE add-users test.user@example.com
-#docker run -v $ROOT/demo:/orderly $OW_CLI_IMAGE grant test.user@example.com */users.manage
 
 # pull dependency images - webapp images may be local only so ignore pull failures
 ADMIN_IMAGE=$1 CONTRIB_IMAGE=$2 docker compose pull --ignore-pull-failures
@@ -44,10 +25,6 @@ docker exec montagu-db-1 montagu-wait.sh 120
 # Migrate the database
 docker pull $MONTAGU_MIGRATE_IMAGE
 docker run --network=montagu_default $MONTAGU_MIGRATE_IMAGE
-
-#start orderly web
-#docker exec montagu-orderly_web-1 mkdir -p /etc/orderly/web
-#docker exec montagu-orderly_web-1 touch /etc/orderly/web/go_signal
 
 # Run packit
 $here/../../../api/scripts/start-packit.sh webapps-packit
