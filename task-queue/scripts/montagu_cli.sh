41718aa6d65c93bc8193213752be661b8bbb20c4
Emma <EmmaRussellKilburn@gmail.com>
2025-10-09 13:59:21 +0100

    set branch in tq cli

--- task-queue/scripts/montagu_cli.sh
+++ task-queue/scripts/montagu_cli.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-image=ghcr.io/vimc/montagu-cli:main
+image=ghcr.io/vimc/montagu-cli:update-repo #TODO: revert when merge branch
 docker pull $image
 exec docker run --rm --network ${NETWORK} $image "$@"
