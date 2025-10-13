4f57fe43bdc8c6ec09dac8a9f800eec6d060e336
Emma <EmmaRussellKilburn@gmail.com>
2025-10-13 17:16:14 +0100

    top level run-all script

---
+++ scripts/run-all.sh
@@ -0,0 +1,125 @@
+#!/usr/bin/env bash
+set -ex
+
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        --api-image)
+            API_IMAGE="$2"
+            shift 2
+            ;;
+          --db-image)
+            DB_IMAGE="$2"
+            shift 2
+            ;;
+          --proxy-image)
+            PROXY_IMAGE="$2"
+            shift 2
+            ;;
+          --task-queue-image)
+            TASK_QUEUE_IMAGE="$2"
+            shift 2
+            ;;
+          --admin-image)
+            ADMIN_IMAGE="$2"
+            shift 2
+            ;;
+          --contrib-image)
+            CONTRIB_IMAGE="$2"
+            shift 2
+            ;;
+          --migrate-image
+            MIGRATE_IMAGE="$2"
+            shift 2
+            ;;
+          --api-config-path
+            API_CONFIG_PATH="$2"
+            shift 2
+            ;;
+          --packit-config
+            PACKIT_CONFIG="$2"
+            shift 2
+            ;;
+        *)
+            echo "Invalid argument: $1"
+            exit 1
+            ;;
+    esac
+done
+
+here=$(dirname $0)
+
+# TODO: pull these into common
+ORG=ghcr.io/vimc
+DEFAULT_BRANCH=update-repo # TODO: change to main when update-repo is merged.
+
+if [[ -z $API_IMAGE ]]; then
+  API_IMAGE=${ORG}/montagu-api:${DEFAULT_BRANCH}
+fi
+
+if [[ -z $DB_IMAGE ]]; then
+  DB_IMAGE=${ORG}/montagu-db:${DEFAULT_BRANCH}
+fi
+
+if [[ -z $PROXY_IMAGE ]]; then
+  PROXY_IMAGE=${ORG}/montagu-proxy:${DEFAULT_BRANCH}
+fi
+
+if [[ -z $TASK_QUEUE_IMAGE ]]; then
+  TASK_QUEUE_IMAGE=${ORG}/task-queue-worker:${DEFAULT_BRANCH}
+fi
+
+if [[ -z $ADMIN_IMAGE ]]; then
+  ADMIN_IMAGE=${ORG}/montagu-admin-portal:${DEFAULT_BRANCH}
+fi
+
+if [[ -z $CONTRIB_IMAGE ]]; then
+  CONTRIB_IMAGE=${ORG}/montagu-contrib-portal:${DEFAULT_BRANCH}
+fi
+
+if [[ -z $MIGRATE_IMAGE ]]; then
+  MIGRATE_IMAGE=${ORG}/montagu-migrate:${DEFAULT_BRANCH}
+fi
+
+# pull dependency images - some images may be local only so ignore pull failures
+API_IMAGE=$API_IMAGE \
+DB_IMAGE=$DB_IMAGE \
+PROXY_IMAGE=$PROXY_IMAGE \
+TASK_QUEUE_IMAGE=$TASK_QUEUE_IMAGE \
+ADMIN_IMAGE=$ADMIN_IMAGE \
+CONTRIB_IMAGE=$CONTRIB_IMAGE \
+MIGRATE_IMAGE=$MIGRATE_IMAGE \
+docker compose pull --ignore-pull-failures
+
+# run docker
+# TODO: is there a nicer way than this!
+API_IMAGE=$API_IMAGE \
+DB_IMAGE=$DB_IMAGE \
+PROXY_IMAGE=$PROXY_IMAGE \
+TASK_QUEUE_IMAGE=$TASK_QUEUE_IMAGE \
+ADMIN_IMAGE=$ADMIN_IMAGE \
+CONTRIB_IMAGE=$CONTRIB_IMAGE \
+MIGRATE_IMAGE=$MIGRATE_IMAGE \
+docker compose --project-name montagu up -d
+
+# Start the APIs
+docker exec montagu-api-1 mkdir -p /etc/montagu/api/
+# TODO: config properties param in webapps client script
+#docker container cp $here/montagu-api.config.properties montagu-api-1:/etc/montagu/api/config.properties
+if [[ -n $API_CONFIG_PATH ]]; then
+  docker container cp $API_CONFIG_PATH montagu-api-1:/etc/montagu/api/config.properties
+fi
+
+docker exec montagu-api-1 touch /etc/montagu/api/go_signal
+
+# Wait for the database
+docker exec montagu-db-1 montagu-wait.sh 120
+
+# Migrate the database
+docker pull $MIGRATE_IMAGE || true # Pull will fail if image is local only
+docker run --network=montagu_default $MIGRATE_IMAGE
+
+# Run packit
+# TODO: webapps-packit param in webapps client script
+# TODO: retire the start-packit script and packit configs in api
+#$here/start-packit.sh webapps-packit
+$here/start-packit.sh $PACKIT_CONFIG
