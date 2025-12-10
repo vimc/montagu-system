65931131438600dba745ad4dbb0119503d738e0c
Wes Hinsley <w.hinsley@imperial.ac.uk>
2025-12-10 16:59:53 +0000

    Manually resolve!

--- deploy/src/montagu_deploy/montagu_constellation.py
+++ deploy/src/montagu_deploy/montagu_constellation.py
@@ -25,6 +25,8 @@ def montagu_constellation(cfg):
     ]
 
     if cfg.use_acme:
+        if cfg.vault and cfg.vault.url:
+            vault.resolve_secrets(cfg.acme_config, cfg.vault.client())
         acme_container = acme.acme_buddy_container(
             cfg.acme_config, "acme-buddy", proxy.name_external(cfg.container_prefix), "montagu-tls", cfg.hostname
         )
